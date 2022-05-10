70c70e42e03f23fdd38cf0bbf0c85f14a82418ba
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-10 14:35:31 -0400

    Task-app working : miss the checkbox action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'tasks#index'
+
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+# get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+# post "/restaurants", to: "restaurants#create", as: :restaurants
+
+# get "/restaurants", to: "restaurants#index"
+# get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+
+# get "/restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+# patch "/restaurants/:id", to: "restaurants#update"
+
+# delete "/restaurants/:id", to: "restaurants#destroy"
 end
